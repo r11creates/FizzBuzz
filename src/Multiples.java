0096f5287a8f145de646c26d72c9ab1f318d7c1a
r11creates <rithvik11sunil@gmail.com>
2024-09-09 14:50:03 -0400

    New file and checkbox

---
+++ src/Multiples.java
@@ -0,0 +1,19 @@
+public class Multiples {
+    public static void main(String[] args) {
+
+        int count = 0; // initalize count
+
+        for (int i = 1; i < 1000; i++){
+            if (i % 3 == 0){
+
+                count++;
+
+            } else if (i % 5 == 0){
+
+                count++;
+
+            }
+        }
+        System.out.println(count);
+    }
+}
