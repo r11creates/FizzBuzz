13472245955ebba0fb434b21a3586f0b11d07d7e
r11creates <rithvik11sunil@gmail.com>
2024-09-09 20:40:36 -0400

    New file and checkbox

---
+++ src/Reduce.java
@@ -0,0 +1,23 @@
+public class Reduce {
+    public static void main(String[] args) {
+
+        int i = 100;
+        int steps = 0;
+
+        while (i > 0) {
+
+            if (i % 2 == 0) {
+
+                i = i / 2;
+
+            } else {
+
+                i = i - 1;
+            }
+
+            steps++;
+        }
+
+        System.out.println(steps);
+    }
+}
